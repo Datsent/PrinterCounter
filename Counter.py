9c67a4ae98246bdbf1ce16cc799d4331e4b7e8c9
Datsent <45486622+Datsent@users.noreply.github.com>
2022-04-24 11:44:39 +0300

    Develop (#1)
    * driver_v98
    
    * proccess_1
    
    * cleare_code
    
    * modify PrinterCounter.py
    
    * ready_to_release
    
    * Add files via upload

---
+++ Counter.py
@@ -0,0 +1,29 @@
+from tkinter import messagebox
+import tkinter
+from Models import Brother, Epson, Samsung
+
+def get_count(ip_adress, model):
+    '''
+    Call specific function to get data.
+    '''
+    if check_printer(model) == 'Brother':
+        return Brother.brother(ip_adress)
+    elif check_printer(model) == 'Epson':
+        return Epson.epson(ip_adress)
+    elif check_printer(model) == 'Samsung':
+        return Samsung.samsung(ip_adress)
+    else:
+        tkinter.messagebox.showerror(title=None, message=f'No program for model {model}')
+
+def check_printer(model):
+    '''
+    Check kind\model of printer.
+    '''
+    if 'brother' in model.lower():
+        return 'Brother'
+    elif 'epson' in model.lower():
+        return 'Epson'
+    elif 'samsung' in model.lower():
+        return 'Samsung'
+    else:
+        tkinter.messagebox.showerror(title=None, message=f'No program for model {model}')
---
+++ DataFile.py
@@ -0,0 +1,40 @@
+import os, sys
+from Utils import *
+from Counter import get_count
+
+def read_data (path):
+    '''
+    Get data from file to list.
+    '''
+    file_printers = open(os.path.join(sys.path[0], path), "r")
+    Lines = file_printers.readlines()
+    file_printers.close()
+    return Lines
+
+def create_new_data(Lines):
+    '''
+    Create new file with collected data from printers.
+    '''
+    new_list = ['#,Model,Serial,IP Adress,Supply Date,Begin Count,Last Count,Count,Total Pages\n']
+    for line in Lines[1:]:
+        line_list = line.split(',')
+        print(f'Calculating Model: {line_list[1]}...')
+        line_list[6] = line_list[7]
+        try:
+            line_list[7] = get_count(line_list[3], line_list[1] )
+            line_list[8] = str(int(line_list[7]) - int(line_list[6])) + '\n'
+            new_list.append(','.join(line_list))
+        except BaseException:
+            print(f"No connection with {line_list[3]}")
+            line_list[7] = '0'
+            line_list[8] = 'N/A\n'
+            new_list.append(','.join(line_list))
+        print(f'Finished Model: {line_list[1]}...')
+    file = open(os.path.join(sys.path[0], ARCHIVE_FILE_PATH), 'w')
+    file.write(''.join(new_list))
+    file.close()
+    file = open(os.path.join(sys.path[0], USE_FILE_PATH), 'w')
+    file.write(''.join(new_list))
+    file.close()
+    print('FINISHED')
+
